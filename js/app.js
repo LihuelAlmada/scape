9e3b5f41f99e6cfb953773fd16fa8c1d6feca460
Necralma <lihuel.almada@gmail.com>
2020-06-26 12:34:32 -0300

    complete game base

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@
     'use strict';
     window.addEventListener('load',init,false);
     var canvas=null,ctx=null;
-    var lastUpdate=0;
     var mousex=0,mousey=0;
+    var lastUpdate=0;
+    var pause=true;
+    var gameover=true;
+    var score=0;
+    var eTimer=0;
     var player=new Circle(0,0,5);
-    var target=new Circle(100,100,10);
+    var bombs=[];
+
+    function random(max){
+        return ~~(Math.random()*max);
+    }
 
     function init(){
         canvas=document.getElementById('canvas');
@@ -13,6 +21,7 @@
         canvas.width=300;
         canvas.height=200;
         
+        enableInputs();
         run();
     }
 
@@ -28,22 +37,64 @@
         paint(ctx);
     }
 
+    function reset(){
+        score=0;
+        eTimer=0;
+        bombs.length=0;
+        gameover=false;
+    }
+
     function act(deltaTime){
-        player.x=mousex;
-        player.y=mousey;
-        
-        if(player.x<0)
-            player.x=0;
-        if(player.x>canvas.width)
-            player.x=canvas.width;
-        if(player.y<0)
-            player.y=0;
-        if(player.y>canvas.height)
-            player.y=canvas.height;
-        
-        if(target.distance(player)>0){
-            var angle=target.getAngle(player);
-            target.move(angle,deltaTime*100);
+        if(!pause){
+            // GameOver Reset
+            if(gameover)
+                reset();
+            
+            // Move player
+            player.x=mousex;
+            player.y=mousey;
+            
+            // Keep player in canvas
+            if(player.x<0)
+                player.x=0;
+            if(player.x>canvas.width)
+                player.x=canvas.width;
+            if(player.y<0)
+                player.y=0;
+            if(player.y>canvas.height)
+                player.y=canvas.height;
+            
+            // Generate new bomb
+            eTimer-=deltaTime;
+            if(eTimer<0){
+                var bomb=new Circle(random(2)*canvas.width,random(2)*canvas.height,10);
+                bomb.timer=1.5+random(2.5);
+                bomb.speed=100+(random(score))*10;
+                bombs.push(bomb);
+                eTimer=0.5+random(2.5);
+            }
+            
+            // Bombs
+            for(var i=0,l=bombs.length;i<l;i++){
+                if(bombs[i].timer<0){
+                    score++;
+                    bombs.splice(i--,1);
+                    l--;
+                    continue;
+                }
+                
+                bombs[i].timer-=deltaTime;
+                var angle=bombs[i].getAngle(player);
+                bombs[i].move(angle,bombs[i].speed*deltaTime);
+                
+                if(bombs[i].timer<0){
+                    bombs[i].radius*=2;
+                    if(bombs[i].distance(player)<0){
+                        gameover=true;
+                        pause=true;
+                    }
+                }
+            }
         }
     }
 
@@ -51,27 +102,54 @@
         ctx.fillStyle='#000';
         ctx.fillRect(0,0,canvas.width,canvas.height);
         
+        for(var i=0,l=bombs.length;i<l;i++){
+            if(bombs[i].timer<0){
+                ctx.fillStyle='#fff';
+                bombs[i].fill(ctx);
+            }
+            else{
+                if(bombs[i].timer<1&&~~(bombs[i].timer*10)%2==0)
+                    ctx.strokeStyle='#fff';
+                else
+                    ctx.strokeStyle='#f00';
+                bombs[i].stroke(ctx);
+            }
+        }
         ctx.strokeStyle='#0f0';
         player.stroke(ctx);
-        ctx.strokeStyle='#f00';
-        target.stroke(ctx);
         
         ctx.fillStyle='#fff';
-        ctx.fillText('Distance: '+player.distance(target).toFixed(1),10,10);
-        ctx.fillText('Angle: '+(player.getAngle(target)*(180/Math.PI)).toFixed(1),10,20);
+        //ctx.fillText('Distance: '+player.distance(bombs[0]).toFixed(1),10,10);
+        //ctx.fillText('Angle: '+(player.getAngle(bombs[0])*(180/Math.PI)).toFixed(1),10,20);
+        ctx.fillText('Score: '+score,10,10);
+        if(pause){
+            ctx.textAlign='center';
+            if(gameover)
+                ctx.fillText('GAME OVER',150,100);
+            else
+                ctx.fillText('PAUSE',150,100);
+            ctx.textAlign='left';
+        }
     }
 
-    document.addEventListener('mousemove',function(evt){
-        mousex=evt.pageX-canvas.offsetLeft;
-        mousey=evt.pageY-canvas.offsetTop;
-    },false);
+    function enableInputs(){
+        document.addEventListener('mousemove',function(evt){
+            mousex=evt.pageX-canvas.offsetLeft;
+            mousey=evt.pageY-canvas.offsetTop;
+        },false);
+        canvas.addEventListener('mousedown',function(evt){
+            pause=!pause;
+        },false);
+    }
 
     function Circle(x,y,radius){
         this.x=(x==null)?0:x;
         this.y=(y==null)?0:y;
         this.radius=(radius==null)?0:radius;
+        this.timer=0;
+        this.speed=0;
     }
-        
+
     Circle.prototype.distance=function(circle){
         if(circle!=null){
             var dx=this.x-circle.x;
@@ -96,6 +174,12 @@
         ctx.beginPath();
         ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
         ctx.stroke();
+    }
+
+    Circle.prototype.fill=function(ctx){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
+        ctx.fill();
     }
 
     window.requestAnimationFrame=(function(){
